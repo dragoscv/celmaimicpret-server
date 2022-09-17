e1c67820a84fd72b09e03e1dfa6ad9201fe63278
Dragos Catalin <dragoscv12@gmail.com>
2022-09-17 23:09:50 +0300

    changes

--- index.js
+++ index.js
@@ -18,16 +18,4 @@ global.gun = gun; /// make global to `node --inspect` - debug only
 
 console.log('Server started on port ' + port + ' with /gun');
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
-
 module.exports = app;
